770bed55d757fdbf174b2162de6774a0a7549e5e
Evgeniy Nazarchuk <evgeniy.nazarchuk@f-secure.com>
2015-12-18 13:45:08 +0300

    Move stream scanning logic to a separate class

--- server/src/main/java/org/antivir/server/AntivirServer.java
+++ server/src/main/java/org/antivir/server/AntivirServer.java
@@ -3,14 +3,12 @@ package org.antivir.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static java.lang.Integer.parseInt;
-import static java.util.Arrays.copyOf;
-import static java.util.Objects.requireNonNull;
+import static org.antivir.server.StreamScanner.StreamScannerBuilder.streamScaner;
+import static org.antivir.server.Utils.print;
 
 public class AntivirServer {
 
@@ -53,9 +51,13 @@ public class AntivirServer {
     try (Reader clientIn = new InputStreamReader(client.getInputStream());
          PrintWriter clientOut = new PrintWriter(new OutputStreamWriter(client.getOutputStream()))) {
 
-      clientOut.println(findInStream(clientIn, INFECTION_MARKER)
-          ? INFECTION_POSITIVE_RESPONSE
-          : INFECTION_NEGATIVE_RESPONSE);
+      streamScaner()
+          .scan(clientIn)
+          .forString(INFECTION_MARKER)
+          .onSuccess(() -> clientOut.println(INFECTION_POSITIVE_RESPONSE))
+          .onFail(() -> clientOut.println(INFECTION_NEGATIVE_RESPONSE))
+          .build()
+          .start();
 
     } catch (IOException e) {
       e.printStackTrace();
@@ -66,105 +68,5 @@ public class AntivirServer {
         e.printStackTrace();
       }
     }
-  }
-
-  private static boolean findInStream(Reader stream, String sample) throws IOException {
-    char[] chunk = new char[BUFFER_SIZE];
-    int charsRead = 0;
-    int matchedCharsCount = 0;
-
-    while ((charsRead = stream.read(chunk)) != -1) {
-      print("chunk read: ", chunk, charsRead);
-      for (int i = 0; i < charsRead; i++) {
-        if (sample.charAt(matchedCharsCount) == chunk[i]) {
-          matchedCharsCount++;
-
-          if (matchedCharsCount == sample.length()) {
-            print("infection found");
-            return true;
-          }
-        } else {
-          i -= matchedCharsCount;
-          matchedCharsCount = 0;
-        }
-      }
-    }
-    print("infection not found");
-    return false;
-  }
-
-//  private static class StreamScanner {
-//    private final Reader stream;
-//    private final String sample;
-//    private final Runnable onSuccess;
-//    private final Runnable onFail;
-//
-//    public StreamScanner(Reader stream, String sample, Runnable onSuccess, Runnable onFail) {
-//      this.stream = requireNonNull(stream);
-//      this.sample = requireNonNull(sample);
-//      this.onSuccess = requireNonNull(onSuccess);
-//      this.onFail = requireNonNull(onFail);
-//    }
-//
-//    public static StreamScanner searchFor(String sample) {
-//      return new StreamScanner(stream);
-//    }
-//
-//    public static class StreamScannerBuilder
-//
-//    public StreamScanner forString(String needle) {
-//      this.sample = needle;
-//      return this;
-//    }
-//
-//    public StreamScanner onSuccess(Runnable cb) {
-//      this.onSuccess = cb;
-//      return this;
-//    }
-//
-//    public StreamScanner onFail(Runnable cb) {
-//      this.onFail = cb;
-//      return this;
-//    }
-//
-//    public void start() throws IOException {
-//      char[] buf = new char[BUFFER_SIZE];
-//      int charsRead = 0;
-//      int matchedCharsCount = 0;
-//
-//      Runnable resultCallback = onFail;
-//      while ((charsRead = stream.read(buf)) != -1) {
-//        print("chunk read: " + String.valueOf(Arrays.copyOf(buf, charsRead)));
-//        if (findInChunk(buf, charsRead, sample, matchedCharsCount)) {
-//          resultCallback = onSuccess;
-//          break;
-//        }
-//      }
-//      resultCallback.run();
-//    }
-//
-//    private boolean findInChunk(char[] chunk, int length, String needle, int matchedCharsCount) {
-//      for (int i = 0; i < length; i++) {
-//        if (needle.charAt(matchedCharsCount) == chunk[i]) {
-//          matchedCharsCount++;
-//
-//          if (matchedCharsCount == needle.length()) {
-//            return true;
-//          }
-//        } else {
-//          i -= matchedCharsCount;
-//          matchedCharsCount = 0;
-//        }
-//      }
-//      return false;
-//    }
-//  }
-
-  private static void print(String string) {
-    System.out.println(string);
-  }
-
-  private static void print(String string, char[] buf, int length) {
-    System.out.println(string + new String(copyOf(buf, length)));
   }
 }
